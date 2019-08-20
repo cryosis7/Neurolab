e4210b58fea187340178f01d09c8e897d13d1a6d
Scott Curtis <scott.curtis@hotmail.com>
2019-08-20 14:44:12 +1200

    cancelling half working

--- app/src/main/java/com/soteria/neurolab/com/soteria/neurolab/reaction_game/ReactionGameActivity.java
+++ app/src/main/java/com/soteria/neurolab/com/soteria/neurolab/reaction_game/ReactionGameActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.soteria.neurolab.R;
@@ -20,6 +21,7 @@ public class ReactionGameActivity extends AppCompatActivity {
     private long startTime = -1;
     private int round = 0;
     private int[] results = new int[5];
+    protected volatile  boolean  isCancelled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,7 @@ public class ReactionGameActivity extends AppCompatActivity {
      */
     public void runRound(View view) {
         view.setVisibility(View.INVISIBLE);
+        isCancelled = false;
         new RandomDelay().execute(this);
         if (((Button) view).getText().equals(getResources().getString(R.string.start_game)))
             ((Button) view).setText(R.string.next_round);
@@ -52,6 +55,7 @@ public class ReactionGameActivity extends AppCompatActivity {
      */
     public void stopTimer(View view) {
         long endTime = System.currentTimeMillis();
+        view.setVisibility(View.INVISIBLE);
         if (startTime == -1)
             throw new IllegalStateException("Attempted to measure elapsed time but start time has not been set.");
 
@@ -61,9 +65,8 @@ public class ReactionGameActivity extends AppCompatActivity {
         round++;
 
         if (round < 5) {
-            view.setVisibility(View.INVISIBLE);
             startTime = -1;
-            findViewById(R.id.activity_reaction_game_start_btn).setVisibility(View.VISIBLE);
+            findViewById(R.id.start_button).setVisibility(View.VISIBLE);
         } else {
             int avgResult = 0;
             for (int x : results)
@@ -79,6 +82,30 @@ public class ReactionGameActivity extends AppCompatActivity {
     }
 
     /**
+     * Resets the round without counting the result.
+     */
+    public void stopTimerFoul() {
+        startTime = -1;
+        ((TextView)findViewById(R.id.information_txt)).setText(R.string.reactionGame_tap_to_early);
+        findViewById(R.id.circle_button).setVisibility(View.INVISIBLE);
+        findViewById(R.id.start_button).setVisibility(View.VISIBLE);
+        isCancelled = true;
+    }
+
+    /**
+     * Used for detecting when the user taps too early or if they tap but don't tap the button
+     * accurately enough.
+     * @param view
+     */
+    public void screenTapped(View view) {
+        View circleButton = findViewById(R.id.circle_button);
+        if (circleButton.getVisibility() == View.VISIBLE)
+            stopTimer(circleButton);
+        else if (findViewById(R.id.start_button).getVisibility() == View.INVISIBLE)
+            stopTimerFoul();
+    }
+
+    /**
      * After a random delay, displays the button.
      */
     private class RandomDelay extends AsyncTask<ReactionGameActivity, Integer, Void> {
@@ -90,11 +117,13 @@ public class ReactionGameActivity extends AppCompatActivity {
             gameReference = new WeakReference<>(params[0]); // Weak reference to prevent memory leaks.
 
             Random random = new Random();
-            int delay = random.nextInt(1000) + 2000;
+            int delay = random.nextInt(6000) + 1000;
             try {
                 Thread.sleep(delay);
-                startTime = System.currentTimeMillis();
-                publishProgress();
+                if (!isCancelled) {
+                    startTime = System.currentTimeMillis();
+                    publishProgress();
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -108,11 +137,8 @@ public class ReactionGameActivity extends AppCompatActivity {
             ReactionGameActivity activity = gameReference.get();
 
             // Due to weak reference, there is a chance the activity does not exist anymore or is closing down.
-            if (activity != null && !activity.isFinishing()) {
-                if (findViewById(R.id.circle_button).getVisibility() == View.INVISIBLE) {
-                    findViewById(R.id.circle_button).setVisibility(View.VISIBLE);
-                }
-            }
+            if (activity != null && !activity.isFinishing())
+                findViewById(R.id.circle_button).setVisibility(View.VISIBLE);
         }
     }
 }
