64d3eae78f7054d9d2e82071efb684e86f2ab4a1
Jason Krieg <jason.krieg@soit.local>
2019-08-22 14:35:35 +1200

    Jasons a Knobhead

---
+++ app/src/main/java/com/soteria/neurolab/SearchAddDeleteActivity.java
@@ -0,0 +1,45 @@
+package com.soteria.neurolab;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SearchAddDeleteActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_search_patient:
+                    mTextMessage.setText(getTitle());
+                    return true;
+                case R.id.navigation_create_patient:
+                    mTextMessage.setText(getTitle());
+                    return true;
+                case R.id.navigation_delete_patient:
+                    mTextMessage.setText(getTitle());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_add_delete_page);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
