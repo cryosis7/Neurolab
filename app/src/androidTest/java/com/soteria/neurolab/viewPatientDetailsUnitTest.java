63629488f859940ea4720676aeec1210c081977d
richarddasanproject <richard.dasan@soit.local>
2019-08-27 11:41:11 +1200

    Code and Unit Tests almost complete

--- app/src/androidTest/java/com/soteria/neurolab/viewPatientDetailsUnitTest.java
+++ app/src/androidTest/java/com/soteria/neurolab/viewPatientDetailsUnitTest.java
@@ -3,6 +3,8 @@ package com.soteria.neurolab;
 import android.util.Log;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
@@ -15,21 +17,108 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.core.AllOf.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
 public class viewPatientDetailsUnitTest
 {
+    /**Establishes the rule for activities*/
     @Rule
-    public ActivityTestRule<viewPatientDetails> rule = new ActivityTestRule<>(viewPatientDetails.class);
+    public ActivityTestRule<viewPatientDetails> activityRule = new ActivityTestRule<>(viewPatientDetails.class);
 
+    /**Establishes the rule for intents*/
+    @Rule
+    public IntentsTestRule<viewPatientDetails> intentsRule = new IntentsTestRule<>(viewPatientDetails.class);
+
+    /**Test to determine whether the delete patient button works as intended by displaying an alert
+     * dialog. This test cancels the action. */
     @Test
-    public void deleteButtonTest()
+    public void deletePatientCancelTest()
     {
         String myDialogText="You are about to delete this patient and all data associated with them.";
         Log.i("@Test", " --- --- --- --- Performing Test - Delete Button --- --- --- ---");
-
         Espresso.onView(withId(R.id.deletePatientButton)).perform(click()).check(matches(allOf(withText(myDialogText), isDisplayed())));
+        Espresso.onView(withId(android.R.id.button2)).perform(click());
+    }
+
+    /**Test to determine whether the delete patient button works as intended by displaying an alert
+     * dialog. This test confirms the deletion of a patient. */
+    /*   TODO replace class in the intended function to its correct one once create patient class is created
+    @Test
+    public void deletePatientConfirmTest()
+    {
+        String myDialogText="You are about to delete this patient and all data associated with them.";
+        Log.i("@Test", " --- --- --- --- Performing Test - Delete Button --- --- --- ---");
+        Espresso.onView(withId(R.id.deletePatientButton)).perform(click()).check(matches(allOf(withText(myDialogText), isDisplayed())));
+        Espresso.onView(withId(android.R.id.button1)).perform(click());
+        intended(hasComponent(TODO Create Patient class name .class.getName()));
+        Intents.release();
+    }   */
+
+    /**Test to determine whether the view report button works as intended by linking the user to the
+     * manage patient page
+       TODO replace class in the intended function to its correct one once manage patient class is created */
+    /*
+    @Test
+    public void transferToManagePatientTest()
+    {
+        Log.i("@Test", " --- --- --- --- Performing Test - Manage Patient Button --- --- --- ---");
+        Espresso.onView(withId(R.id.managePatientButton)).perform(click());
+        intended(hasComponent(TODO Manage patient class name .class.getName()));
+        Intents.release();
+    } */
+
+    /**Test to determine whether the view report button works as intended by linking the user to the
+     * view report page
+       TODO replace class in the intended function to its correct one once patient profile class is created */
+    /*
+    @Test
+    public void transferToViewReportTest()
+    {
+        Log.i("@Test", " --- --- --- --- Performing Test - View Report Button --- --- --- ---");
+        Espresso.onView(withId(R.id.viewReportButton)).perform(click());
+        intended(hasComponent(TODO View Report class name .class.getName()));
+        Intents.release();
+    } */
+
+    /**Test to determine whether the run games button works as intended by linking the user to the
+     * patient profile page
+       TODO replace class in the intended function to its correct one once patient profile class is created */
+    /*
+    @Test
+    public void transferToPatientProfileTest()
+    {
+        Log.i("@Test", " --- --- --- --- Performing Test - Run Games Button --- --- --- ---");
+        Espresso.onView(withId(R.id.runGamesButton)).perform(click());
+        intended(hasComponent(TODO Patient Profile class name .class.getName()));
+        Intents.release();
+    } */
+
+    /**Test to determine whether the log out button in the overflow menu works as intended by linking
+     * the user to the log in page
+       TODO replace class in the intended function to its correct one once log in class is created */
+    /*
+    @Test
+    public void transferToLogInScreenTest()
+    {
+        Log.i("@Test", " --- --- --- --- Performing Test - Overflow Menu Log Out --- --- --- ---");
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withId(R.id.action_logout)).perform(click());
+        intended(hasComponent(TODO Log In class name .class.getName()));
+        Intents.release();
+    } */
+
+    /**Test to determine whether the disclaimer button in the overflow menu works as intended by
+     * displaying the disclaimer in an alert dialog. Also tests the continue button inside the dialog*/
+    @Test
+    public void showDisclaimerTest()
+    {
+        String disclaimerText = "Neurolab by Soteria is designed as an assistant tool only.";
+        Log.i("@Test", " --- --- --- --- Performing Test - Overflow Menu Disclaimer --- --- --- ---");
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withId(R.id.action_disclaimer)).perform(click()).check(matches(allOf(withText(disclaimerText), isDisplayed())));
+        Espresso.onView(withId(android.R.id.button1)).perform(click());
     }
 }
--- app/src/main/java/com/soteria/neurolab/viewPatientDetails.java
+++ app/src/main/java/com/soteria/neurolab/viewPatientDetails.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +20,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class viewPatientDetails extends AppCompatActivity {
 
     @Override
-    //TODO Remove comment in the function parameters once information can be passed through
     protected void onCreate(Bundle savedInstanceState) {
 
         //TODO Hardcoded values, remove after parameters can be passed through
@@ -27,36 +27,29 @@ public class viewPatientDetails extends AppCompatActivity {
         Date lastDayPlayed = new Date();
 
         /*
-        Values that are passed through from the previous screen.
-
+        TODO Values that are passed through from the previous screen. Update these when needed.
         Bundle viewBundle = getIntent.getExtras();
         String patientIdentifier = bundle.getString("pID");
         Date lastDayPlayed = bundle.getDate("pDate");
         */
 
+        //Update the support action bar to set the back button and title to appropriate values
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle("Patient Details");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_view_patient_details);
 
-
-        //Information for changing the text information on load
-        //TODO pass through patient identifier and last game run information when completed
-
+        /*Code for changing the patientID and lastGamesRun text views to display the information
+          passed through to it from another page. */
         final TextView patientID = findViewById(R.id.patientIDTitleTextView);
         final TextView lastPlayed = findViewById(R.id.patientGamesLastRunTextView);
+        String inputDate = new SimpleDateFormat("dd-mm-yyyy hh:mm a", Locale.ENGLISH).format(lastDayPlayed);
+        patientID.setText(getString(R.string.view_patient_details_patient_identifier, patientIdentifier));
+        lastPlayed.setText(getString(R.string.view_patient_details_last_date_played, inputDate));
 
-        patientID.setText("Patient: " + patientIdentifier );
-
-        DateFormat convertDate = new SimpleDateFormat("dd-mm-yyyy hh:mm a");
-        String inputDate = convertDate.format(lastDayPlayed);
-        lastPlayed.setText("Games last run: " + inputDate );
-
-
-        //Button instructions for run games button
-        //TODO add link to send users to patient home once created, remove toast once done.
-
+        /*When run games is pressed, sends user to the patient profile
+        TODO add link to send users to patient home once created, remove toast and comment markers  once done. */
         final Button runButton = findViewById(R.id.runGamesButton);
         runButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -69,10 +62,9 @@ public class viewPatientDetails extends AppCompatActivity {
             }
         });
 
-
-        //Button instructions for manage patient button
-        //TODO add link to send users to the manage patient screen once created, remove toast once done.
-
+        /*When manage patient button is pressed, send the user to the manage patient screen for the
+          current patient.
+          TODO add link to send users to the manage patient screen once created, remove toast and comment markers  once done. */
         final Button manageButton = findViewById(R.id.managePatientButton);
         manageButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -85,10 +77,9 @@ public class viewPatientDetails extends AppCompatActivity {
             }
         });
 
-
-        //Button instructions for view report button
-        //TODO add link to send users to the view report screen once created, remove toast once done.
-
+        /*When the view report button is pressed, send the user to the report screen for the current
+          patient.
+          TODO add link to send users to the view report screen once created, remove toast and comment markers once done. */
         final Button reportButton = findViewById(R.id.viewReportButton);
         reportButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -101,53 +92,64 @@ public class viewPatientDetails extends AppCompatActivity {
             }
         });
 
-
-        //Button instructions for delete patient
-        //TODO add link to search patient and remove patient from database once created, remove toast once done.
-
+        /*When the delete patient button is pressed, ask the user for confirmation. If they agree,
+          remove the patient from the database and send the user to the create patient page.
+          TODO add link to search patient and remove patient from database once created, remove toast once done. */
         final Button deleteButton = findViewById(R.id.deletePatientButton);
         deleteButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                //Build the alert dialog warning the user of their action.
                 AlertDialog.Builder deleteBuilder = new AlertDialog.Builder(viewPatientDetails.this);
                 deleteBuilder.setTitle("Deleting Patient");
                 deleteBuilder.setMessage("You are about to delete this patient and all data associated with them.\n\n" +
                         "This action cannot be undone.\n\nDo you wish to continue?");
+                //If delete is pressed, delete the patient and send the user to the search patients screen
                 deleteBuilder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         //TODO enter code to remove patient from database here
+                        dialogInterface.cancel();
                         Toast.makeText(getApplicationContext(),"DELETE PRESSED: Removed patient, send to search",Toast.LENGTH_SHORT).show();
                         //startActivity(new Intent(this, //TODO add link to search patient here));
-                        dialogInterface.cancel();
                     }
                 });
+                //If cancel is pressed, close the alert dialog.
                 deleteBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.cancel();
                     }
                 });
-                AlertDialog deleteConfirm = deleteBuilder.create();
+                final AlertDialog deleteConfirm = deleteBuilder.create();
+                deleteConfirm.setOnShowListener( new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface arg0 ) {
+                        deleteConfirm.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+                    }
+                });
                 deleteConfirm.show();
             }
         });
 
-
-        //Button instructions for the home image button
-        //TODO add link to search patient once created, remove toast once done.
-
+        /*When the home button is pressed, send the user to the search patient screen
+          TODO add link to search patient once created, remove toast once done. */
         final ImageButton homeButton = findViewById(R.id.homeButton);
         homeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"HOME BUTTON PRESSED: Sending to search patients screen",Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(this, //TODO add link to search patient here));
+                /*startActivity(new Intent(this, TODO add link to search patient here));
+                  finish();*/
             }
         });
     }
 
+    /** Sets the actions bar to include an overflow menu.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
+        /*Grabs the information from the overflow_menu resource in the menu folder and sets them
+          to the action bar*/
         try {
             getMenuInflater().inflate(R.menu.overflow_menu, menu);
             return true;
@@ -157,18 +159,18 @@ public class viewPatientDetails extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        Toast.makeText(getApplicationContext(),"BACK BUTTON PRESSED: Sending to search patients screen",Toast.LENGTH_SHORT).show();
-    }
-
+    /** Determines the behaviour of options selected from the action bar.
+     *  TODO insert link for logout and database functions for removal
+     */
     public boolean onOptionsItemSelected(MenuItem menu)
     {
         try {
              switch(menu.getItemId()) {
+                 //If the back button is pressed, return the user to the last visited page
                  case android.R.id.home:
                      super.onBackPressed();
                      return true;
+                 //If the disclaimer button is pressed, display the disclaimer in an alert dialog
                  case R.id.action_disclaimer:
                      final AlertDialog.Builder disclaimerBuilder = new AlertDialog.Builder(this );
                      disclaimerBuilder.setTitle("Disclaimer");
@@ -176,6 +178,7 @@ public class viewPatientDetails extends AppCompatActivity {
                              "It should not be relied upon for treatment, diagnosis or professional medical advice.\n" +
                              "If you have any queries about its use, please consult your local physician or health care provider.\n\n" +
                              "By accessing and using this app, you have confirmed that you have read, understood and accepted the above statement.");
+                     //If the confirm button is pressed, close the dialog
                      disclaimerBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener()  {
                          @Override
                          public void onClick(DialogInterface dialogInterface, int i) {
@@ -183,6 +186,7 @@ public class viewPatientDetails extends AppCompatActivity {
                          }
                      });
                      final AlertDialog showDisclaimer = disclaimerBuilder.create();
+                     //Change the button colour of the alert dialog to be the primary colour
                      showDisclaimer.setOnShowListener( new DialogInterface.OnShowListener() {
                          @Override
                          public void onShow(DialogInterface arg0 ) {
@@ -191,12 +195,17 @@ public class viewPatientDetails extends AppCompatActivity {
                      });
                      showDisclaimer.show();
                      return true;
+                  //If the log out button is pressed, send the user to the log in screen
                  case R.id.action_logout:
                      //TODO link to logout, remove toast afterwards
                      Toast.makeText(getApplicationContext(), "LOGOUT PRESSED - Going to log in screen", Toast.LENGTH_SHORT).show();
-                     //startActivity(new Intent(this, //TODO add link to log in here));
+                     /*startActivity(new Intent(this, //TODO add link to log in here));
+                       finish();
+                     */
                      return true;
+                  //If an unknown option is selected, display an error to the user
                  default:
+                     Toast.makeText(getApplicationContext(), "INVALID - Option not valid or completed", Toast.LENGTH_SHORT).show();
                      return false;
              }
         } catch(Exception e) {
