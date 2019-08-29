ef9803610b7d58f593a1a9e1201e34ba7cb5cae8
Jason Krieg <jason.krieg@soit.local>
2019-08-29 12:01:05 +1200

    created the activity for view report

---
+++ app/src/main/java/com/soteria/neurolab/ViewReportActivity.java
@@ -0,0 +1,14 @@
+package com.soteria.neurolab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_report);
+    }
+}
