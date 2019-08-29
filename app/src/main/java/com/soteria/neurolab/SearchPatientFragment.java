c3c77f9d61c2d2d796c2ec886869b00454d60cc0
bbwinx <briannawinkels@hotmail.com>
2019-08-30 10:55:32 +1200

    search patient GUI

--- app/src/main/java/com/soteria/neurolab/SearchPatientFragment.java
+++ app/src/main/java/com/soteria/neurolab/SearchPatientFragment.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 
 /**
@@ -47,6 +49,13 @@ public class SearchPatientFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View view =  inflater.inflate(R.layout.fragment_search_patient, container, false);
+
+        //Initialise UI elements
+        final SearchView inputPatientID = view.findViewById(R.id.search_patient_SearchView);
+        final ListView patientListView = view.findViewById(R.id.search_patient_ListView);
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_patient, container, false);
     }
