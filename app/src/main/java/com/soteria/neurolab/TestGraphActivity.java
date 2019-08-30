d80c7f3b198e2cc04bd54872f3d20b6afcf44526
Scott Curtis <scott.curtis@hotmail.com>
2019-08-31 00:31:31 +1200

    Changes for the night.

--- app/src/main/java/com/soteria/neurolab/TestGraphActivity.java
+++ app/src/main/java/com/soteria/neurolab/TestGraphActivity.java
@@ -1,18 +1,32 @@
 package com.soteria.neurolab;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.soteria.neurolab.database.DatabaseAccess;
 import com.soteria.neurolab.models.GameSession;
+import com.soteria.neurolab.utilities.DateManager;
 
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class TestGraphActivity extends AppCompatActivity {
+
+    private static final String TAG = "Scott_Debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +41,31 @@ public class TestGraphActivity extends AppCompatActivity {
         LineChart chart = findViewById(R.id.reportChart);
 
         List<Entry> entries = new ArrayList<>();
-        for (GameSession session : gameSessions) {
-            final String date = session.getDate();
-
-//            entries.add(new Entry(session.getMetrics(), ))
+        try {
+            for (GameSession session : gameSessions) {
+                Date date = DateManager.convertToDate(session.getDate());
+                entries.add(new Entry(date.getTime(), (float) session.getMetrics()));
+            }
         }
+        catch (ParseException ex) {
+           Log.e(TAG, "ParseException was thrown when adding converting dates from String into Date\n" + ex.toString());
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Reaction Game Metrics");
+        dataSet.setColor(R.color.colorBlack);
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        formatChart(chart);
+        chart.invalidate();
+    }
+
+    private void formatChart(LineChart chart) {
+        chart.setDrawBorders(true);
+        chart.setBorderColor(getResources().getColor(R.color.colorPrimaryDark));
+        chart.setBorderWidth(2);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setDrawAxisLine(false);
+//        xAxis.setdr
     }
 }
---
+++ app/src/main/java/com/soteria/neurolab/utilities/DateManager.java
@@ -0,0 +1,19 @@
+package com.soteria.neurolab.utilities;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateManager {
+
+    /**
+     * Converts a date string into a Date type.
+     * Returns Null if there was a parsing error.
+     * @param dateString the date string to be converted. It must be in yyyy-mm-dd format.
+     * @return Date The date object if parsing was successful; null otherwise.
+     */
+    public static Date convertToDate(String dateString) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return simpleDateFormat.parse(dateString);
+    }
+}
