04593beab191bf8f974bb45aebb74e58fd52ffe7
Sheogorath Daedric Prince Of Madness <jasonkriegproject@gmail.com>
2019-09-21 19:16:54 +1200

    Finished testing delete page

--- app/src/androidTest/java/com/soteria/neurolab/DeletePatientFragmentTest.java
+++ app/src/androidTest/java/com/soteria/neurolab/DeletePatientFragmentTest.java
@@ -6,17 +6,23 @@ import android.view.View;
 
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.soteria.neurolab.database.DatabaseAccess;
+import com.soteria.neurolab.models.GameAssignment;
+import com.soteria.neurolab.models.Patient;
 
 import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -25,8 +31,12 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.assertNotNull;
 
@@ -41,14 +51,17 @@ public class DeletePatientFragmentTest {
     //Selects the delete patient fragment for testing, also empties the database except for games and adds in test data
     @Before
     public void setupFragment(){
+        //Setup test data for all tests
         db = new DatabaseAccess(InstrumentationRegistry.getInstrumentation().getTargetContext());
-        onView(withId(R.id.navigation_delete_patient)).perform(click());
-        onView(withId(R.id.deletePatientFragment)).check(matches(isDisplayed()));
-        Log.i("@Before", "setupFragment: Delete patient fragment selected from bottom navigation menu");
         db.deleteAllPatients();
         db.deleteAllSessions();
         db.deleteAllAssignments();
         createTestPatients();
+
+        //set fragment
+        onView(withId(R.id.navigation_delete_patient)).perform(click());
+        onView(withId(R.id.deletePatientFragment)).check(matches(isDisplayed()));
+        Log.i("@Before", "setupFragment: Delete patient fragment selected from bottom navigation menu");
     }
 
 
@@ -128,15 +141,62 @@ public class DeletePatientFragmentTest {
 
     @Test
     public void testDeleteNoPatients(){
-        //This is code for selecting a checkbox in the recycler view, I am yet to work out how to de-select them
-//        onView(withId(R.id.deletePatient_searchRecycler))
-//                .perform(RecyclerViewActions.actionOnItemAtPosition(0,
-//                        MyViewAction.clickChildViewWithId(R.id.delete_patient_recycler_checkbox)));
-
         onView(withId(R.id.deletePatient_deleteButton)).perform(click());
 
         onView(withText(R.string.delete_patient_no_patients_selected)).inRoot(MobileViewMatchers.isToast()).check(matches(isDisplayed()));
 
+        onView(withId(R.id.deletePatient_searchRecycler)).check(matches(allOf(
+                isDisplayed(), hasChildCount(4)
+        )));
+    }
+
+
+    @Test
+    public void testDeleteAllPatients(){
+        onView(withId(R.id.deletePatient_searchRecycler)).check(matches(allOf(
+                isDisplayed(), hasChildCount(4)
+        )));
+
+        onView(withId(R.id.deletePatient_selectAllCheckBox)).perform(click());
+
+        onView(withId(R.id.deletePatient_deleteButton)).perform(click());
+
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.deletePatient_searchRecycler)).check(matches(allOf(
+                isDisplayed(), hasChildCount(0)
+        )));
+
+        //Check Patient table in database
+        List<Patient> patients = db.getAllPatients();
+        assertThat(patients.size(), is(0));
+    }
+
+
+    @Test
+    public void testDeleteOnePatient(){
+        onView(withId(R.id.deletePatient_searchRecycler)).check(matches(allOf(
+                isDisplayed(), hasChildCount(4)
+        )));
+
+        onView(withId(R.id.deletePatient_searchRecycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,
+                        MyViewAction.clickChildViewWithId(R.id.delete_patient_recycler_checkbox)));
+
+        onView(withId(R.id.deletePatient_deleteButton)).perform(click());
+
+        onView(withText(R.string.delete_patient_confirmation_dialog_1_selected)).check(matches(allOf
+                (withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE), withText(R.string.delete_patient_confirmation_dialog_1_selected))));
+
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.deletePatient_searchRecycler)).check(matches(allOf(
+                isDisplayed(), hasChildCount(3)
+        )));
+
+        //Check Patient table in database
+        List<Patient> patients = db.getAllPatients();
+        assertThat(patients.size(), is(3));
     }
 
 
@@ -147,6 +207,10 @@ public class DeletePatientFragmentTest {
         db.createPatient("BW03");
     }
 
+
+    /**
+     * Helper class for testing elements inside a recycler view
+     */
     public static class MyViewAction {
 
         public static ViewAction clickChildViewWithId(final int id) {
@@ -168,7 +232,5 @@ public class DeletePatientFragmentTest {
                 }
             };
         }
-
     }
-
 }
