0e5e3d5604092aad5a102bcc612c74011c67b2d5
Scott Curtis <scott.curtis@hotmail.com>
2019-09-23 15:09:11 +1200

    small changes

--- app/src/main/java/com/soteria/neurolab/LoginCreatePasswordActivity.java
+++ app/src/main/java/com/soteria/neurolab/LoginCreatePasswordActivity.java
@@ -1,18 +1,12 @@
 package com.soteria.neurolab;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import com.soteria.neurolab.utilities.DisclaimerAlertDialog;
 
 public class LoginCreatePasswordActivity extends AppCompatActivity {
     private static final String TAG = "LoginCreatePasswordActivity";
@@ -26,47 +20,11 @@ public class LoginCreatePasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_create_password);
-        switchFragment(); // TODO: Could this be inline instead of function call or is it reused
+        switchFragment();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        /*Grabs the information from the overflow_menu resource in the menu folder and sets them
-          to the action bar*/
-        try {
-            getMenuInflater().inflate(R.menu.overflow_menu, menu);
-            return true;
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "EXCEPTION " + e + " occurred!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-    }
-
-    // TODO: Remove unneeded settings.
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                finish();
-                return true;
-            case R.id.action_disclaimer:
-                DisclaimerAlertDialog dad = new DisclaimerAlertDialog();
-                dad.showDisclaimer(this, getResources());
-                return true;
-            //If the log out button is pressed, send the user to the log in screen
-            case R.id.action_logout:
-                startActivity(new Intent(this, LoginCreatePasswordActivity.class));
-                finish();
-                return true;
-            //If an unknown option is selected, display an error to the user
-            default:
-                Toast.makeText(getApplicationContext(), "INVALID - Option not valid or completed", Toast.LENGTH_SHORT).show();
-                return false;
-        }
-    }
-
-    // TODO: Handle back button pressed.
+    // TODO: Handle back button pressed. Check when logged out from main screen.
+    // Try inserting this into the intent: intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
     void switchFragment() {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
--- app/src/main/java/com/soteria/neurolab/LoginFragment.java
+++ app/src/main/java/com/soteria/neurolab/LoginFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.soteria.neurolab.utilities.DisclaimerAlertDialog;
 import com.soteria.neurolab.utilities.PasswordAuthentication;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -76,6 +77,7 @@ public class LoginFragment extends Fragment {
         PasswordAuthentication authenticator = new PasswordAuthentication();
         if (storedHash != null) {
             if (authenticator.authenticate(password, storedHash)) {
+                new DisclaimerAlertDialog().showDisclaimerWithCancel(getContext(), getResources());
                 startActivity(new Intent(getContext(), SearchCreateDeleteActivity.class));
             } else {
                 ((TextInputLayout) getView().findViewById(R.id.login_password_inputLayout)).setError("Incorrect Password");
