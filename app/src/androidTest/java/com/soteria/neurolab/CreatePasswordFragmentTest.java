28b255c7c32165646c33503dbd1d1a1080ff7d34
Scott Curtis <scott.curtis@hotmail.com>
2019-09-26 17:00:16 +1200

    updating

--- app/src/androidTest/java/com/soteria/neurolab/CreatePasswordFragmentTest.java
+++ app/src/androidTest/java/com/soteria/neurolab/CreatePasswordFragmentTest.java
@@ -17,11 +17,18 @@ import static android.content.Context.MODE_PRIVATE;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasBackground;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isFocusable;
+import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
 import static androidx.test.espresso.matcher.ViewMatchers.withChild;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.not;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -44,8 +51,11 @@ public class CreatePasswordFragmentTest {
     public void testAllElementsLoad() {
         // Is everything displayed on the screen.
         onView(allOf(withId(R.id.login_neurolab_logo), isDisplayed())).check(matches(isCompletelyDisplayed()));
-        onView(withChild(withId(R.id.create_password_button))).check(matches(isCompletelyDisplayed()));
-        onView(withChild(withId(R.id.login_password_label))).check(matches(isCompletelyDisplayed()));
-
+        onView(withId(R.id.create_password_label)).check(matches(allOf(withText("Create A New Password"), isDisplayed())));
+        onView(withId(R.id.create_password_edit_text_1)).check(matches(allOf(withHint("Enter Password"), isFocusable(), isDisplayed(), not(isSelected()))));
+        onView(withId(R.id.create_password_edit_text_2)).check(matches(allOf(withHint("Re-Enter Password"), isFocusable(), isDisplayed(), not(isSelected()))));
+        onView(withId(R.id.create_password_disclaimer_checkbox)).check(matches(allOf(not(isChecked()), isClickable(), withText("I agree to the disclaimer"), isDisplayed())));
+        onView(withId(R.id.create_password_button)).check(matches(allOf(withText("Create Password"), isClickable(), isDisplayed())));
+//        onView(withId(R.id.create_password_button)).check(matches(isCompletelyDisplayed()));
     }
 }
