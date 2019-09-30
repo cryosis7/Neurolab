47cd2e582e6639e69abdda59bd31ea418342ef76
RichardDasanProject <richarddasanproject@gmail.com>
2019-09-30 20:17:23 +1300

    FInished for the night

--- app/src/main/java/com/soteria/neurolab/LoginActivity.java
+++ app/src/main/java/com/soteria/neurolab/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.soteria.neurolab;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -42,6 +45,12 @@ public class LoginActivity extends AppCompatActivity {
         signIn();
     }
 
+    /**
+     * This function is called whenever the "Reset Password" button is pressed
+     * @param view the Reset Password button
+     */
+    public void forgotPasswordButtonHandler(View view) { forgotPassword(); }
+
     private void signIn() {
         TextInputEditText passwordEditText = findViewById(R.id.login_password_edittext);
         char[] password = passwordEditText.getText().toString().toCharArray();
@@ -69,7 +78,52 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This function is called whenever the "I forgot my Password" button is pressed. It will
+     * check to see whether they have any security questions set and if they do, will redirect
+     * them to the LoginSecurityQuestions page. If they do not, they will be prompted to do a database
+     * purge and a password reset at the same time. Otherwise they can keep attempting to log in.
+     */
     private void forgotPassword() {
+        //Grabs the SharedPreferences file
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(getString(R.string.shared_preferences_filename), MODE_PRIVATE);
 
+        //Checks to see whether the SharedPreferences file has the security questions inside of them
+        //TODO FIND ERROR
+        if(!pref.contains("QUESTION_ONE") && pref.contains("QUESTION_TWO")) {
+            //If the security questions do not exist, call an alertDialog to ask if user wants to
+            //wipe their password and the database in order to get back into the app
+            final AlertDialog.Builder eraseBuilder = new AlertDialog.Builder(this);
+            eraseBuilder.setTitle("Warning");
+            eraseBuilder.setMessage(getString(R.string.login_erase_database_warning));
+            eraseBuilder.setPositiveButton("Erase", new DialogInterface.OnClickListener()  {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //Call database purge here
+                }
+            });
+            eraseBuilder.setNegativeButton("Decline", new DialogInterface.OnClickListener()  {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            final AlertDialog showErase = eraseBuilder.create();
+            showErase.setOnShowListener( new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface arg0) {
+                    showErase.getButton(AlertDialog.BUTTON_POSITIVE).setTextSize(20);
+                    showErase.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorWarning));
+                    showErase.getButton(AlertDialog.BUTTON_NEGATIVE).setTextSize(20);
+                }
+            });
+            showErase.show();
+            TextView bodyText = showErase.findViewById(android.R.id.message);
+            bodyText.setTextSize(24);
+        } else {
+            //If the security questions do exist, direct the user to the LoginSecurityQuestions
+            //page
+            startActivity(new Intent(this, LoginSecurityQuestions.class));
+        }
     }
 }
